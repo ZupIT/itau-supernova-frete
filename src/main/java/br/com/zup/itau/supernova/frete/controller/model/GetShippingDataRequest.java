24a13231a495a62e04707f4bb7ca221afc48a7eb
Gabriel Bobrov <gabriel.bobrov@zup.com.br>
2025-08-05 16:32:34 -0300

    fix: fix skus validation

--- src/main/java/br/com/zup/itau/supernova/frete/controller/model/GetShippingDataRequest.java
+++ src/main/java/br/com/zup/itau/supernova/frete/controller/model/GetShippingDataRequest.java
@@ -11,7 +11,7 @@ public record GetShippingDataRequest(
         @JsonProperty("skuList")
         @Size(min = 1, max = 1)
         @NotNull
-        List<String> skuList,
+        List<@Size(min = 1, max = 36) String> skuList,
 
         @JsonProperty("zipCode")
         @NotBlank
